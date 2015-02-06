868bc7fb2eff4d1c43fc89100e86be23209fd0fa
Umesh Adtani <adtani@gmail.com>
2015-02-06 13:29:49 +0530

    Added diagnostic headers ...

--- diy/src/main/java/com/springbootstrapper/filters/CORSHeadersFilter.java
+++ diy/src/main/java/com/springbootstrapper/filters/CORSHeadersFilter.java
@@ -10,10 +10,16 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import com.springbootstrapper.config.AppConfig;
 
 @Component
 public class CORSHeadersFilter implements Filter {
+	
+	@Autowired
+	private AppConfig config;
 
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
@@ -23,6 +29,14 @@ public class CORSHeadersFilter implements Filter {
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type");
 		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Expose-Headers", "Location");
+		
+		//Diagnostics Filters ...
+		response.setHeader("spring.datasource.url",config.getProperty("spring.datasource.url"));
+		response.setHeader("spring.datasource.username",config.getProperty("spring.datasource.username"));
+		response.setHeader("spring.datasource.password",config.getProperty("spring.datasource.password"));
+		response.setHeader("spring.datasource.driverClassName",config.getProperty("spring.datasource.driverClassName"));
+		response.setHeader("logging.file",config.getProperty("logging.file"));
+		response.setHeader("filevault.root",config.getProperty("filevault.root"));
 		chain.doFilter(req, res);
 	}
 
