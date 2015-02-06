c8cfd1d341efdd542c2f6bf4f53d15ff8a67e055
Umesh Adtani <adtani@gmail.com>
2015-02-06 16:49:39 +0530

    app.config sysouts added ...

--- diy/src/main/java/com/springbootstrapper/config/AppConfig.java
+++ diy/src/main/java/com/springbootstrapper/config/AppConfig.java
@@ -17,6 +17,16 @@ public class AppConfig {
     @Autowired
     protected AppConfig(AbstractBeanFactory beanFactory) {
         this.beanFactory = beanFactory;
+        System.out.println("#######################################################################");
+        System.out.println("AppConfig v1.0 Initialized with beanFactory!");
+		System.out.println("app.version"+" = " +getProperty("1.0.0"));
+		System.out.println("spring.datasource.url"+" = " +getProperty("spring.datasource.url"));
+		System.out.println("spring.datasource.username"+" = " +getProperty("spring.datasource.username"));
+		System.out.println("spring.datasource.password"+" = " +getProperty("spring.datasource.password"));
+		System.out.println("spring.datasource.driverClassName"+" = " +getProperty("spring.datasource.driverClassName"));
+		System.out.println("logging.file"+" = " +getProperty("logging.file"));
+		System.out.println("filevault.root"+" = " +getProperty("filevault.root"));        
+        System.out.println("#######################################################################");
     }
 
     public  String getProperty(String key) {
